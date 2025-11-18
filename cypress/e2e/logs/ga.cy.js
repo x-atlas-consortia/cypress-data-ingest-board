c3be142bca4427ec9dd6fa6fa30a25a10530e215
Lisa-Ann B <lisabruney@pitt.edu>
2025-11-18 10:33:50 -0500

    Test report

--- cypress/e2e/logs/ga.cy.js
+++ cypress/e2e/logs/ga.cy.js
@@ -1,4 +1,5 @@
 import {MSGS, PATHS, DATA, SEL, WAIT} from "../../config/constants";
+import 'cypress-iframe';
 
 describe(`${MSGS.name}.Usage.GA`, () => {
 
@@ -9,6 +10,31 @@ describe(`${MSGS.name}.Usage.GA`, () => {
 
     it('Loads report', () => {
         cy.get('iframe').should('have.length', 1)
+    })
+
+    describe('Check report content', () => {
+    it('Elements in iframe loads and are interactive', () => {
+
+        cy.get('.ant-menu-submenu').eq(0).as('gaMenu')
+        cy.get('@gaMenu').find('.ant-menu-title-content').click()
+        cy.get('@gaMenu').find('.ant-menu-sub li').each(($el, index, collection) => {
+
+            cy.log($el.text())
+            $el.trigger('click')
+
+            cy.frameLoaded('.ant-layout iframe');
+
+            // Interact with elements inside the iframe
+            cy.iframe('.ant-layout iframe').within(() => {
+
+            cy.wait(2000)
+            //cy.get('ng2-date-range-picker button').click()
+            cy.contains('Active users').should('be.visible')
+            cy.contains('Export to PDF').should('be.visible')
+            });
+        })
+        
+    });
     })
 
 
